dc20ba8be4f6475efad4543b958be12a615d64ee
Caroline Tan <carolinectan3@gmail.com>
2021-06-09 14:51:08 -0400

    Add passenger methods

---
+++ lib/passenger.rb
@@ -0,0 +1,26 @@
+class Passenger
+  attr_reader :name, :age
+
+  def initialize(info)
+    @name = info['name']
+    @age = info['age']
+    @drive = false
+  end
+
+  def adult?
+    #considered an adult if they are age 18 or older.
+    if @age >= 18
+      true
+    else
+      false
+    end
+  end
+
+  def driver?
+    @drive
+  end
+
+  def drive
+    @drive = true
+  end
+end
