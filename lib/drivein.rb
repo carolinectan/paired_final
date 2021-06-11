d9521aca96b8055e8fead9755daf0407bd62af9f
Caroline Tan <carolinectan3@gmail.com>
2021-06-11 12:24:30 -0400

    Create drivein class and add methods

---
+++ lib/drivein.rb
@@ -0,0 +1,26 @@
+class Drivein
+  attr_reader :name, :vehicles
+
+  def initialize(name)
+    @name = name
+    @vehicles = []
+  end
+
+  def park_vehicle(vehicle)
+    @vehicles << vehicle
+  end
+
+  def vehicles_with_multiple_passengers
+    @vehicles.select do |vehicle|
+      vehicle.passengers.length > 1
+    end
+  end
+
+  def passengers_under_18
+    @vehicles.flat_map do |vehicle|
+      vehicle.passengers.select do |passenger|
+        passenger.adult? == false
+      end
+    end
+  end
+end
